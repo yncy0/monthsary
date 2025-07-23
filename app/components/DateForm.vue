eb454c928355317b504e08c9df81bc0a340e6cb5
Yncy0 <yncyncy0@gmail.com>
2025-07-23 15:29:43 +0800

    finished: forms being submitted through backend from DateForm.vue

--- app/components/DateForm.vue
+++ app/components/DateForm.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { Database } from "~/types/database.types.ts";
+
 const { userAuth } = useAuthState();
 
 // FIXME: bad design
@@ -11,16 +13,45 @@ const meetingTime = ref<string | undefined>(undefined);
 async function onFormSubmit() {
   let insert
 
-  if (moodSpectrum.value && place.value && foodEstablishment.value && meetingTime.value) {
-    insert = useInsertDatePlan(
-      moodSpectrum.value,
-      place.value,
-      foodEstablishment.value,
-      meetingTime.value,
-    );
-
+  try {
+    if (moodSpectrum.value && place.value && foodEstablishment.value && meetingTime.value) {
+      insert = await useInsertDatePlan(
+        moodSpectrum.value,
+        place.value,
+        foodEstablishment.value,
+        meetingTime.value,
+      );
+    }
+  } catch (error) {
+    console.error(error)
   }
   return insert;
+}
+
+async function onFormSubmitTest() {
+  const client = useSupabaseClient<Database>()
+
+  try {
+    const { data, error } = await client
+      .from("date_plan")
+      .insert(
+        {
+          mood_spectrum: moodSpectrum.value,
+          place: place.value,
+          food_establishment: foodEstablishment.value,
+          meeting_time: meetingTime.value,
+        }
+      )
+      .select()
+
+    if (data) console.log(data)
+
+    if (error) throw error
+
+  } catch (error) {
+    console.error(error)
+  }
+
 }
 </script>
 
@@ -29,29 +60,16 @@ async function onFormSubmit() {
     <UFormField label="How's your day?">
       <UInput v-model="moodSpectrum" placeholder="Answer Please" class="w-full" :ui="{ base: 'py-3' }" />
     </UFormField>
-    <UFormField v-model="place" label="Where do you want to go out?">
-      <UInput placeholder="SM MOA, Boracay, Baguio, etc..." class="w-full" :ui="{ base: 'py-3' }" />
+    <UFormField label="Where do you want to go out?">
+      <UInput v-model="place" placeholder="SM MOA, Boracay, Baguio, etc..." class="w-full" :ui="{ base: 'py-3' }" />
     </UFormField>
-    <UFormField v-model="foodEstablishment" label="Where do you want to eat?">
-      <UInput placeholder="Vikings, Jollibee, McDonalds, etc..." class="w-full" :ui="{ base: 'py-3' }" />
+    <UFormField label="Where do you want to eat?">
+      <UInput
+v-model="foodEstablishment" placeholder="Vikings, Jollibee, McDonalds, etc..." class="w-full"
+        :ui="{ base: 'py-3' }" />
     </UFormField>
-    <UFormField v-model="meetingTime" label="What time should we met?">
-      <TimeFieldRoot 
-        id="birthday" 
-        v-slot="{ segments }"
-        class="w-36 flex select-none bg-white items-center rounded-lg shadow-sm text-center text-green10 border p-1 data-[invalid]:border-red-500">
-        <template v-for="item in segments" :key="item.part">
-          <TimeFieldInput v-if="item.part === 'literal'" :part="item.part">
-            {{ item.value }}
-          </TimeFieldInput>
-          <TimeFieldInput 
-            v-else 
-            :part="item.part"
-            class="rounded p-0.5 focus:outline-none focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-green9">
-            {{ item.value }}
-          </TimeFieldInput>
-        </template>
-      </TimeFieldRoot>
+    <UFormField label="What time should we met?">
+      <UInput v-model="meetingTime" type="time" />
     </UFormField>
 
     <USeparator class="py-4" />
--- app/composables/insert/useInsertDatePlan.ts
+++ app/composables/insert/useInsertDatePlan.ts
@@ -1,12 +1,5 @@
 import type { Database } from "~/types/database.types.ts";
 
-export type DatePlan = {
-  moodSpectrum: string;
-  place: string;
-  foodEstablishment: string;
-  meetingTime: string;
-};
-
 export async function useInsertDatePlan(
   moodSpectrum: string,
   place: string,
@@ -14,17 +7,14 @@ export async function useInsertDatePlan(
   meetingTime: string,
 ) {
   const client = useSupabaseClient<Database>();
-
   const { data, error } = await client
-    .from("data_plan")
-    .insert([
-      {
-        mood_spectrum: moodSpectrum,
-        place: place,
-        food_establishment: foodEstablishment,
-        meeting_time: meetingTime,
-      },
-    ])
+    .from("date_plan")
+    .insert({
+      mood_spectrum: moodSpectrum,
+      place: place,
+      food_establishment: foodEstablishment,
+      meeting_time: meetingTime,
+    })
     .select();
 
   if (error) throw error;
