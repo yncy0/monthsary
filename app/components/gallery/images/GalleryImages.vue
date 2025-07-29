5a881033862dc4db1063e610839464882d9b5569
Yncy0 <yncyncy0@gmail.com>
2025-07-30 02:37:56 +0800

    update: structureing GalleryImages.vue for fetching data based on date; update: added another function in useFetchImage.ts; added: seed.sql; update: database types

--- app/components/gallery/images/GalleryImages.vue
+++ app/components/gallery/images/GalleryImages.vue
@@ -17,18 +17,37 @@ onMounted(async () => {
   <Suspense>
     <ul class="flex flex-row flex-wrap w-full object-cover gap-5 lg:gap-2 px-6 lg:px-24">
       <li v-for="(image, index) in imageUrls" :key="index">
-        <NuxtImg v-if="!userAuth" v-slot="{ src, isLoaded, imgAttrs }" :src="image" alt="It is an image" :custom="true"
-          class="flex-1 w-full lg:w-64 h-96 rounded-md">
+        <NuxtImg 
+          v-if="!userAuth" 
+          v-slot="{ src, isLoaded, imgAttrs }" 
+          :src="image"
+          alt="It is an image" 
+          :custom="true"
+          class="flex-1 w-full lg:w-64 h-96 rounded-md"
+        >
           <img v-if="isLoaded" v-bind="imgAttrs" :src="src">
 
           <USkeleton v-else class="w-full lg:w-64 h-96" />
         </NuxtImg>
-        <NuxtImg v-if="userAuth" v-slot="{ src, isLoaded, imgAttrs }" :src="image.image_url" alt="It is an image"
-          :custom="true" class="w-full lg:w-64 h-96 rounded-md">
-          <img v-if="isLoaded" v-bind="imgAttrs" :src="src">
 
-          <USkeleton v-else class="w-full lg:w-64 h-96" />
-        </NuxtImg>
+        <template v-if="userAuth">
+          <li v-for="items in imageUrls">
+            {{}}
+          </li>
+          <!--
+          <NuxtImg 
+            v-slot="{ src, isLoaded, imgAttrs }" 
+            :src="image.image_url"
+            alt="It is an image"
+            :custom="true"
+            class="w-full lg:w-64 h-96 rounded-md"
+          >
+            <img v-if="isLoaded" v-bind="imgAttrs" :src="src">
+
+            <USkeleton v-else class="w-full lg:w-64 h-96" />
+          </NuxtImg>
+          -->
+          </template>
       </li>
     </ul>
   </Suspense>
