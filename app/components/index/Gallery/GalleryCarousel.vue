62d5c78b0f5615e016b8e4acf9864e63352f64e8
Yncy0 <yncyncy0@gmail.com>
2025-08-10 21:06:16 +0800

    update: check if the user exist via state management rather than prop drilling

--- app/components/index/Gallery/GalleryCarousel.vue
+++ app/components/index/Gallery/GalleryCarousel.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts" generic="T">
+<script setup lang="ts">
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
+import type { CarouselItem } from "@nuxt/ui"
+
+const { userAuth } = useAuthState()
+
 const props = defineProps<{
-  items: T[];
-  isUser: boolean;
+  items: CarouselItem[];
 }>();
 
 dayjs.extend(utc);
@@ -23,7 +26,7 @@ dayjs.extend(timezone);
     :ui="{ item: 'basis-full sm:basis-1/2 md:basis-1/3' }"
   >
     <GalleryCard
-      v-if="props.isUser"
+      v-if="userAuth"
       :img-src="item.images.image_url"
       :description="item.description"
       :header="dayjs(item.date).tz('Asia/Manila').format('MMMM DD, YYYY')"
